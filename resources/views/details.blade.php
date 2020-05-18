d79f37153089d55d25602c8738e49dd6c9aec45d
foxtsune <juliopeluciofox@hotmail.com>
2020-05-18 10:14:34 -0300

    Initial commit

---
+++ resources/views/details.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Pizzas - {{$id}}
+        </div>
+    </div>
+</div>
+@endsection
